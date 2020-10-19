861c651646cc07705638a6bc068f48b36f4e83f8
Ivan <gibson.ivan@gmail.com>
2020-10-19 13:16:29 -0400

    First Commit of Get Me A Game after the precious site was far to gone

---
+++ COMP2084GetMeAGame/Controllers/ShopController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2084GetMeAGame.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084GetMeAGame.Controllers
+{
+    public class ShopController : Controller
+    {
+        // DB connection
+        private readonly ApplicationDbContext _context;
+        //connect to db whenever controller is used
+        public ShopController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            //get list of categories to sidplay to customers on the main shopping page
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View();
+        }
+    }
+}
