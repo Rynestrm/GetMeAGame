c11368ac62467b56e305dba7f84e684ed367bc50
Ivan <gibson.ivan@gmail.com>
2020-11-30 12:57:34 -0500

    added test project added unit testing for product controller

---
+++ COMP2084GetMeAGame/Controllers/DummiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084GetMeAGame.Controllers
+{
+    public class DummiesController : Controller
+    {
+        //constructor method
+        public DummiesController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
---
+++ GetMeAGameTest/DummiesControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using COMP2084GetMeAGame.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetMeAGameTest
+{
+    [TestClass]
+    public class DummiesControllerTest
+    {
+        [TestMethod]
+        public void IndexReturnsSomething()
+        {
+            //Arrange
+            var controller = new DummiesController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexLoadsIndexView()
+        {
+            //arrange
+            var controller = new DummiesController();
+
+            //act
+            var result = (ViewResult)controller.Index();
+
+            //assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+    }
+}
